f01f8685de36cc448a88eef2149ee12dc0d055a1
hagrace4 <hagrace4@gmail.com>
2023-02-01 19:30:47 -0800

    Add operate function

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -13,3 +13,18 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(op, a, b) {
+  if (op == "+") {
+    return add(a, b);
+  }
+  if (op == "-") {
+    return subtract(a, b);
+  }
+  if (op == "*") {
+    return multiply(a, b);
+  }
+  if (op == "/") {
+    return divide(a, b);
+  }
+}
