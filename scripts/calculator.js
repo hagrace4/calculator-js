55520ad8839540fb09cdb8e68d2f03578693d3e6
hagrace4 <hagrace4@gmail.com>
2023-02-05 15:10:46 -0800

    Re create the calculator.js

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,3 +1,13 @@
+
+// **variables**
+let displayValue = "";
+// set user input to display
+const userInput = document.querySelector('.user-input');
+
+// Event listener for buttons
+
+
+
 // Operation Functions
 function add(a, b) {
   return a + b;
@@ -28,118 +38,4 @@ function operate(op, a, b) {
   if (op == "/") {
     return divide(a, b);
   }
-}
-
-// **variables**
-let displayValue = "";
-// set user input to display
-const userInput = document.querySelector('.user-input');
-
-// Event listeners for buttons
-const sevenBtn = document.getElementById("seven");
-sevenBtn.addEventListener("click", () => {
-  displayValue = sevenBtn.textContent;
-  userInput.textContent = displayValue;
-});
-
-
-const eightBtn = document.getElementById("eight");
-eightBtn.addEventListener("click", () => {
-  displayValue = eightBtn.textContent;
-  userInput.textContent = displayValue;
-});
-
-const nineBtn = document.getElementById("nine");
-nineBtn.addEventListener("click", () => {
-  displayValue = nineBtn.textContent;
-  userInput.textContent = displayValue;
-});
-
-const addBtn = document.getElementById("add");
-addBtn.addEventListener("click", () => {
-  displayValue = addBtn.textContent;
-  userInput.textContent = displayValue;
-});
-
-const fourBtn = document.getElementById("four");
-fourBtn.addEventListener("click", () => {
-  displayValue = fourBtn.textContent;
-  userInput.textContent = displayValue;
-});
-
-const fiveBtn = document.getElementById("five");
-fiveBtn.addEventListener("click", () => {
-  displayValue = fiveBtn.textContent;
-  userInput.textContent = displayValue;
-});
-
-const sixBtn = document.getElementById("six");
-sixBtn.addEventListener("click", () => {
-  displayValue = sixBtn.textContent;
-  userInput.textContent = displayValue;
-});
-
-const subtractBtn = document.getElementById("subtract");
-subtractBtn.addEventListener("click", () => {
-  displayValue = subtractBtn.textContent;
-  userInput.textContent = displayValue;
-});
-
-const oneBtn = document.getElementById("one");
-oneBtn.addEventListener("click", () => {
-  displayValue = oneBtn.textContent;
-  userInput.textContent = displayValue;
-});
-
-const twoBtn = document.getElementById("two");
-twoBtn.addEventListener("click", () => {
-  displayValue = twoBtn.textContent;
-  userInput.textContent = displayValue;
-});
-
-const threeBtn = document.getElementById("three");
-threeBtn.addEventListener("click", () => {
-  displayValue = threeBtn.textContent;
-  userInput.textContent = displayValue;
-});
-
-const multiplyBtn = document.getElementById("multiply");
-multiplyBtn.addEventListener("click", () => {
-  displayValue = multiplyBtn.textContent;
-  userInput.textContent = displayValue;
-});
-
-const zeroBtn = document.getElementById("zero");
-zeroBtn.addEventListener("click", () => {
-  displayValue = zeroBtn.textContent;
-  userInput.textContent = displayValue;
-});
-
-let firstNumber = 0;
-let secondNumber = 0;
-let operator = "";
-let result = 0;
-
-// doCalc() Function
-// Function to calculate the result
-function handleNumber(event) {
-  // Get the target element and its text content
-  const target = event.target;
-  const text = target.textContent;
-
-  // Get the current value of the display
-  const display = document.querySelector(".user-input");
-  let displayValue = display.textContent;
-
-  // check if the display value is currently showing an error message
-  if (displayValue == "Error") {
-    //if so, clear the display to empty string
-    displayValue = "";
-  }
-
-  // Append the pressed number to the display value
-  displayValue += text;
-
-  // Update the display with the new value
-  display.textContent = displayValue;
 }
