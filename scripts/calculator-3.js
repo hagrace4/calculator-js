9b81fbd588a303710309ece1e12fe58433c75564
hagrace4 <hagrace4@gmail.com>
2023-02-24 20:15:51 -0800

    Change colors of the calculator

--- scripts/calculator-3.js
+++ scripts/calculator-3.js
@@ -9,6 +9,10 @@ let result = null;
 const buttons = document.querySelectorAll('button');
 const display = document.getElementById('display');
 
+function updateDisplay() {
+  display.innerText = displayValue;
+}
+
 
 
 // Event Listener for number buttons
@@ -35,14 +39,32 @@ numButtons.forEach(button => {
 const opButtons = document.querySelectorAll('.operator');
 opButtons.forEach(button => {
   button.addEventListener('click', () => {
-    if (firstOperator == null) {
-      //handle first operand clicks
-      console.log('operator1 clicked'); //debug
-      firstOperand = displayValue;
-      firstOperator = button.value;
-    } else if (firstOperator != null && secondOperator== null) {
-      console.log('operator2 clicked'); //debug
-      //handle input and storing of secondOperand
-    }
+    displayValue += button.value;
+    updateDisplay();
   });
 });
+
+//handle clear button press
+const clearButton = document.querySelectorAll('.clear, .all-clear');
+clearButton.forEach(button => {
+  button.addEventListener('click', () => {
+    clearDisplay();
+    updateDisplay();
+  })
+})
+
+// Select the equals button and add an event listener
+const equalsButton = document.querySelector('.equals');
+equalsButton.addEventListener('click', () => {
+  console.log('equals clicked!');
+  const result = math.evaluate(displayValue);
+  displayValue = result;
+  updateDisplay();
+});
+
+
+function clearDisplay() {
+  displayValue = "0";
+  result = null;
+  updateDisplay();
+}
